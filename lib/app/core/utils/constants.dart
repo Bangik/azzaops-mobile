f303c1f963ef86b44d8ecbbd9cbcdeab02cc9ed7
bangik <aldi.novaldi.frmnsyah@gmail.com>
2026-08-04 07:34:33 +0700

    Update baseUrl in Constants to the production API endpoint

--- lib/app/core/utils/constants.dart
+++ lib/app/core/utils/constants.dart
@@ -1,7 +1,6 @@
 class Constants {
   // Use 10.0.2.2 for Android emulator to access localhost, or change to host IP
-  static const String baseUrl =
-      'https://implicitly-genuine-kiwi.ngrok-free.app/api/v1';
+  static const String baseUrl = 'https://app.azzakarunia.my.id/api/v1';
   static const int connectTimeout = 30; // seconds
   static const int receiveTimeout = 30; // seconds
 }
